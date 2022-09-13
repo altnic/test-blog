4cee8dcdcf344b917eef37fd0431ca59cb7e5b86
Nikolay Ponomarev <n.s.ponomarev@yandex.ru>
2022-09-13 14:20:45 +0700

    init: start project

---
+++ src/scripts/index.js
@@ -0,0 +1,11 @@
+// import './import/modules';
+// import './import/components';
+
+// import $ from 'jquery'
+
+// $(document).ready(() => {
+// 	alert('JS is work')
+// 	console.log('JS is work')
+// })
+
+console.log("JS is work");
